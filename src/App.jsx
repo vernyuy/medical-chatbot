8dcce459f6b4ec7c22382f0ee0377cd8866a0da9
vernyuy <fonchu.e.venyuy@gmail.com>
2025-01-20 09:22:32 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,128 +1,17 @@
-// import { useEffect, useState } from 'react'
-// import './App.css'
-// import { Interactions } from '@aws-amplify/interactions';
-// import { Amplify } from 'aws-amplify';
-// import { Button, withAuthenticator } from '@aws-amplify/ui-react';
-// import '@aws-amplify/ui-react/styles.css';
-// import axios from 'axios';
-// // import config from './amplifyconfiguration.json';
-
-// // Amplify.configure(config);
+/* eslint-disable react/prop-types */
 
 
-// function App(
-//   // { signOut, user }
-// ) {
-
-//   const [lexRes, setLexRes] = useState([]);
-//   let questions = []
-//   const [userInput, setUserInput] = useState("");
-//   const [messages, setMessages] = useState([]);
-
-//   const [letsChat, setLetsChat] = useState(false);
-
-//   const handleInputChange = (event) => {
-//     setUserInput(event.target.value);
-//   };
-
-//   const handleKeyPress = (event) => {
-//     if (event.key === "Enter") {
-//       setMessages([...messages, userInput]);
-//       questions = [...questions, userInput]
-//       sendReq(userInput, 'session1')
-//       console.log(questions);
-//       setUserInput("");
-//     }
-//   };
-
-
-//   const sendReq = async (message, sId) => {
-//     axios.post(
-//       "https://yfjyuskkn8.execute-api.us-east-1.amazonaws.com/demo",
-//       {
-//         message: message,
-//         sessionId: sId,
-//       }
-//     ).then((res)=>{
-//       console.log(res)
-//       setLexRes(res.data.message)
-//       setLetsChat(true)
-//     })
-// };
-//   const chat = async (userInput) => {
-//     console.log("I'm ccallled")
-//     const response = await Interactions.send({
-//       botName: "MedicalChatbot",
-//       message: userInput,
-//     });
-//     console.log(response)
-//     setLexRes(response.messages[0].content)
-//     setLetsChat(true)
-//   }
-
-//   return (
-//     <>
-//       <div>
-//         {/* <div className='user'>
-//           <div>Hi!<h3>{user?.username}</h3></div>
-//           <button onClick={signOut}>Sign out</button>
-//         </div> */}
-//         <div className="chat-container">
-//           <div className="chat-box">
-
-//             <h1 className="chat-title">Welcome To AnyCompany Health</h1>
-//             <div>
-//               <div className="messages-container">
-//                 {messages.map((message, index) => (
-//                   <div key={index} className="message">
-//                     {message}
-//                   </div>
-//                 ))}
-//               </div>
-//             </div>
-//             <div>
-//               {
-//                 lexRes
-//               }
-//             </div>
-//             {
-//               letsChat ? <>
-//                 <div className="chat-input">
-//                   <input
-//                     type="text"
-//                     placeholder="AnyCompany Health Chatbot "
-//                     className="chat-textbox"
-//                     value={userInput}
-//                     onChange={handleInputChange}
-//                     onKeyPress={handleKeyPress}
-//                   />
-//                   <div className="chat-icons">
-//                     <button className="icon-button">📎</button>
-//                     <button className="icon-button">🎁</button>
-//                     <button className="icon-button">🎙️</button>
-//                   </div>
-//                 </div>
-//               </> : <button onClick={() => sendReq('Hello', 'session1')}>Start Chat</button>
-//             }
-//           </div>
-//         </div>
-//       </div>
-//     </>
-//   )
-// }
-
-import { useEffect, useState } from 'react'
+import {  useState } from 'react'
 import './App.css'
-import { Interactions } from '@aws-amplify/interactions';
 import { Amplify } from 'aws-amplify';
-import { Button, withAuthenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-// import config from './amplifyconfiguration.json';
+import config from './amplifyconfiguration.json';
 import axios from 'axios';
-// Amplify.configure(config);
+Amplify.configure(config);
 
 function App(
-  // { signOut, user }
+  { signOut, user }
 ) {
 
   
@@ -169,8 +58,8 @@ function App(
     <>
       <div>
         <div className='user'>
-          {/* <div>Hi!<h3>{user?.username}</h3></div>
-          <button onClick={signOut}>Sign out</button> */}
+          <div>Hi!<h3>{user?.username}</h3></div>
+          <button onClick={signOut}>Sign out</button>
         </div>
         <div className="chat-container">
           <div className="chat-box">
@@ -221,7 +110,5 @@ function App(
 }
 
 
-// export default withAuthenticator(
-export default  App
-// );
+export default withAuthenticator(App);
 
